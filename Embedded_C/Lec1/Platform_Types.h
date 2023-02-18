a9c13b9a2a05426b58d9a3792fdfb46b5163be9d
Omar Nasser <omarnasser@gmail.com>
2023-02-18 22:42:24 +0200

    Typedefs

---
+++ Embedded_C/Lec1/Platform_Types.h
@@ -0,0 +1,16 @@
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+
+
+typedef signed char				sint8_t
+typedef unsigned char			uint8_t
+typedef signed short			sint16_t
+typedef unsigned short			uint16_t
+typedef signed long				sint32_t
+typedef unsigned long			uint32_t
+typedef signed long long		sint64_t
+typedef unsigned long long		uint64_t
+typedef float					float32_t;
+typedef double					float64_t;
+
+#endif
